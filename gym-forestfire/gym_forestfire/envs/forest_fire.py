51bc49d54ebc61e1428a64911fde29f54e768f8f
Travis Hammond <dashdeckers@gmail.com>
2019-03-25 00:28:05 +0100

    BIG update: added comments, updated features + observation space, changed burning cells to list

--- gym-forestfire/gym_forestfire/envs/forest_fire.py
+++ gym-forestfire/gym_forestfire/envs/forest_fire.py
@@ -8,20 +8,6 @@ WIDTH = 20
 HEIGHT = 10
 
 """
-> Features need to return:
-
-For each agent:
-    The position (x, y) of the agent
-    For the furthest fire element in each direction (N, S, E, W):
-        The distance between the fire and the agent
-        The angle to the fire from the agent
-The total number of burning cells
-
-Resulting in a list of length: (# agents) * 10 + 1
-
-This change needs to be reflected in the observation space
-
-
 > Q-Table approach:
 
 All simple implementation use a pre-initialized matrix as a Q-Table,
@@ -47,26 +33,32 @@ class ForestFire(gym.Env):
     metadata = {'render.modes' : ['human']}
 
     def __init__(self):
+        # set the random seed for reproducability
         r.seed(0)
+        # the action space consists of 6 discrete actions
         self.action_space = spaces.Discrete(6)
-        # feature vector: [distance_to NF, angle_to NF, x, y, #b_cells]
-        self.observation_space = spaces.Box(low=np.array([0, -math.pi, 0, 0, 0]),
-                high=np.array([WIDTH+HEIGHT, math.pi, WIDTH, HEIGHT, WIDTH*HEIGHT]),
-                dtype=np.float32)
+        # the observation space consists of 14 continuous and discrete values
+        # see features for more information
+        (max_ob, min_ob) = self.get_max_min_obs()
+        self.observation_space = spaces.Box(low=min_ob, high=max_ob, dtype=np.float32)
+        # the environment is a 2D array of elements, plus an agent
         self.env = Environment(WIDTH, HEIGHT)
 
-    # If the action is not handled, the agent does nothing
+    # take action and update the environment
     def step(self, action):
         if action in ["N", "S", "E", "W"] or action in range(4):
             self.env.agents[0].move(action)
         if action in ["D", 4]:
             self.env.agents[0].dig()
+        # If the action is not handled, the agent does nothing
         self.env.update()
         return [self.env.get_features(), self.env.get_fitness(), self.env.running, {}]
 
+    # resets environment to default values
     def reset(self):
         self.env.reset_env()
 
+    # prints an ascii map of the environment
     def render(self, mode='human', close=False):
         for y in range(self.env.height):
             for x in range(self.env.width):
@@ -86,6 +78,7 @@ class ForestFire(gym.Env):
                     print("O", end="")
             print("")
 
+    # prints information on windspeed and direction
     def wind_info(self):
         if (self.env.wind_vector[0] == 0 and self.env.wind_vector[1] == 0) or self.env.wind_speed == 0:
             print("No wind!")
@@ -102,31 +95,69 @@ class ForestFire(gym.Env):
             print("Wind direction: " + wind_direction)
             print("Wind speed: ", self.env.wind_speed)
 
+    """
+    Returns the minimum and maximum possible observation values.
+    This does not generalize to n_agents, only works for 1 agent!
+    """
+    def get_max_min_obs():
+        min_ob = np.zeros(14)
+        max_ob = np.zeros(14)
+        # x coordinate
+        min_ob[0] = 0
+        max_ob[0] = WIDTH
+        # y coordinate
+        min_ob[1] = 0
+        max_ob[1] = HEIGHT
+        # distance and angle for each of the 4 directions
+        for idx in range(2, 10):
+            if idx % 2 == 0:
+                min_ob[idx] = 0
+                max_ob[idx] = WIDTH + HEIGHT
+            else:
+                min_ob[idx] = math.pi * (-1)
+                max_ob[idx] = math.pi
+        # num burning cells
+        min_ob[10] = 0
+        max_ob[10] = WIDTH * HEIGHT
+        # windspeed
+        min_ob[11] = 0
+        max_ob[11] = 3
+        # wind x
+        min_ob[12] = 0
+        max_ob[12] = 1
+        # wind y
+        min_ob[13] = 0
+        max_ob[13] = 1
+        return min_ob, max_ob
+
 
 class Environment:
     def __init__(self, width, height):
         self.running = True
 
-        self.wind_speed = r.randint(0, 3)
-        self.wind_vector = (r.randint(-1, 1), r.randint(-1, 1))
-
+        # environment variables
         self.width = width
         self.height = height
         self.world = self.create_world(width, height)
 
-        self.burning_cells = set()
-        self.barriers = set()
-        self.agents = list()
+        # wind variables
+        self.wind_speed = r.randint(0, 3)
+        self.wind_vector = (r.randint(-1, 1), r.randint(-1, 1))
 
-        self.total_fuel = self.get_total_fuel()
+        # book-keeping variables
+        self.burning_cells = list()
+        self.agents = list()
         self.fuel_burnt = 0
 
+        # create an agent and set the fire
         self.add_agent_at(1, 1)
         self.set_fire_at("center")
 
+    # resets the environment by calling the initializer again
     def reset_env(self):
         self.__init__(self.width, self.height)
 
+    # creates a simple map of grass
     def create_world(self, width, height):
         world = list()
         for x in range(width):
@@ -136,10 +167,11 @@ class Environment:
             world.append(line)
         return world
 
+    # puts an agent at (x, y)
     def add_agent_at(self, x, y):
         self.agents.append(Agent(x, y, self))
 
-    # takes an (x, y) tuple or a string
+    # takes an (x, y) tuple or a string and sets that element on fire
     def set_fire_at(self, pos):
         if pos == "center":
             (x, y) = (int(self.width/2), int(self.height/2))
@@ -147,28 +179,46 @@ class Environment:
             (x, y) = pos
         burning_cell = self.world[x][y]
         burning_cell.burning = True
-        self.burning_cells.add(burning_cell)
+        self.burning_cells.append(burning_cell)
 
+    # returns the element at (x, y)
     def get_at(self, x, y):
         return self.world[x][y]
 
+    # sets the element at (x, y) to a new element
     def set_at(self, x, y, cell):
         self.world[x][y] = cell
 
+    # checks if (x, y) is within bounds
     def inbounds(self, x, y):
         return x >= 0 and x < self.width and y >= 0 and y < self.height
 
+    # checks if the cell at (x, y) is traversable
     def traversable(self, x, y):
         cell = self.get_at(x, y)
         return cell.type in ["Grass", "Dirt"] and not cell.burning
 
+    """
+    Main update loop:
+
+    Checks if any agents are on burning cells and should die
+
+    Keeps count of the total amount of fuel burnt up
+
+    Spreads the fire:
+        Reduces fuel of burning cells, removing them if burnt out
+        Applies heat from burning cells to neighbouring cells
+        If any neighbouring cells ignite, add them to burning cells
+
+    If there are no more agents or burning cells, set running to false
+    """
     def update(self):
-        agents_to_remove = list()
+        agents_to_remove = set()
         for agent in self.agents:
             status = agent.update()
             if status == "Dead":
-                agents_to_remove.append(agent)
-        self.agents = [a for a in self.agents if not a in agents_to_remove]
+                agents_to_remove.add(agent)
+        self.agents = list(set(self.agents) - agents_to_remove)
 
         self.fuel_burnt += len(self.burning_cells)
 
@@ -185,12 +235,11 @@ class Environment:
                         status = n_cell.get_heat_from(cell, self)
                         if status == "Ignited":
                             cells_to_add.add(n_cell)
-        self.burning_cells.difference_update(cells_to_remove)
-        self.burning_cells.update(cells_to_add)
 
         if not self.agents or not self.burning_cells:
             self.running = False
 
+    # returns the total amount of fuel available on the map
     def get_total_fuel(self):
         total_fuel = 0
         for x in range(self.width):
@@ -198,31 +247,52 @@ class Environment:
                 total_fuel += self.get_at(x, y).fuel
         return total_fuel
 
+    # returns the amount of fuel already burnt as a negative number
     def get_fitness(self):
         return (-1) * self.fuel_burnt
 
-    # returns: [distance_to, angle_to, x, y] of closest fire + [#burning cells]
+    # returns the most N, S, E, and W burning cells
+    def get_corner_points_of_fire(self):
+        N = S = E = W = self.burning_cells[0]
+        for fire in self.burning_cells:
+            if fire.x < W.x:
+                W = fire
+            if fire.x > E.x:
+                E = fire
+            if fire.y < N.y:
+                N = fire
+            if fire.y > S.y:
+                S = fire
+        return (N, S, E, W)
+
+    """
+    Returns:
+    For each agent:
+        The position (x, y) of the agent
+        For the furthest fire element in each direction (N, S, E, W):
+            The distance between the fire and the agent
+            The angle to the fire from the agent
+    The total number of burning cells
+    The windspeed and direction
+
+    Resulting in a list of length: (# agents) * 10 + 4
+    """
     def get_features(self):
         if not self.burning_cells:
-            return [-1, -1, -1, -1, 0]
+            return [-1] * (len(self.agents) * 10 + 4)
+
         features = list()
-
         for agent in self.agents:
-            min_distance = 1000
-            closest_fire = None
+            features += [agent.x, agent.y]
+            corner_points = self.get_corner_points_of_fire()
+            for fire in corner_points:
+                features.append(fire.distance_to(agent, "euclidean"))
+                features.append(fire.angle_to(agent))
 
-            for cell in self.burning_cells:
-                distance = cell.distance_to(agent, "euclidean")
-                if distance < min_distance:
-                    min_distance = distance
-                    closest_fire = cell
+        features.append(len(self.burning_cells))
+        features.append(self.wind_speed)
+        features += list(self.wind_vector)
 
-            (ax, ay) = agent.get_pos()
-            features.append(distance)
-            features.append(closest_fire.angle_to(agent, self))
-            features.append(ax)
-            features.append(ay)
-            features.append(len(self.burning_cells))
         return features
 
 
@@ -231,20 +301,22 @@ class Element:
         self.x = x
         self.y = y
 
+    # define hashvalue for set compatibility
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    # define equality for set compatibility
+    def __eq__(self, other):
+        try:
+            return (self.x, self.y) == (other.x, other.y)
+        except AttributeError as e:
+            return NotImplemented
+
+    # get position of element
     def get_pos(self):
         return (self.x, self.y)
 
-    def get_color(self):
-        if self.fuel <= 0:
-            return "Black"
-        if self.burning:
-            return "Red"
-        if self.temp > 0.75 * self.threshold:
-            return "Orange"
-        if self.temp > 0.5 * self.threshold:
-            return "Yellow"
-        return self.color
-
+    # reduces fuel by 1 if it is burning, returns "Burnt out" if fuel reaches 0
     def time_step(self):
         if self.burning:
             self.fuel -= 1
@@ -254,6 +326,16 @@ class Element:
                 return "Burnt Out"
         return "No Change"
 
+    """
+    Gets heat from other cell if the other cell is burning
+
+    Formula: cell.heat * (wind_speed * angle_with_wind + distance_to_cell)^-1
+
+    The closer and the more in wind direction to the burning cell, the more
+    heat is transfered (maximum heat is equal to cell.heat)
+
+    Returns "Ignited" if the ignition threshold is reached
+    """
     def get_heat_from(self, cell, env):
         if not cell.burnable:
             return "No Change"
@@ -269,6 +351,7 @@ class Element:
             return "Ignited"
         return "No Change"
 
+    # returns the distance to the other cell according to the metric specified
     def distance_to(self, cell, metric):
         if metric == "manhattan":
             return abs(self.x - cell.x) + abs(self.y - cell.y)
@@ -276,16 +359,20 @@ class Element:
             return math.sqrt(math.pow(self.x - cell.x, 2)
                             + math.pow(self.y - cell.y, 2))
 
+    # returns the angle between the vector (cell, other_cell) and the wind vector
     def wind_angle_to(self, cell, env):
         (cx, cy) = (self.x - cell.x, self.y - cell.y)
         (wx, wy) = env.wind_vector
         return abs(math.atan2(wx*cy - wy*cx, wx*cx + wy*cy))
 
-    def angle_to(self, cell, env):
+    # returns the angle between the vector (cell, other_cell) and (0, 1)
+    def angle_to(self, cell):
         (cx, cy) = (self.x - cell.x, self.y - cell.y)
         (rx, ry) = (0, 1)
         return abs(math.atan2(rx*cy - ry*cx, rx*cx + ry*cy))
 
+    # returns the neighbours: every cell that can be reached by taking
+    # cell.r cityblock/manhattan steps from the origin cell is a neighbour
     def get_neighbours(self, env):
         neighbours = set()
         for x in range(self.r + 1):
@@ -325,6 +412,7 @@ class Grass(Element):
         self.threshold = 5
         self.fuel = 5
 
+
 class Dirt(Element):
     def __init__(self, x, y):
         super().__init__(x, y)
@@ -346,9 +434,22 @@ class Agent:
         self.y = y
         self.env = env
 
+    # define hashvalue for set compatibility
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    # define equality for set compatibility
+    def __eq__(self, other):
+        try:
+            return (self.x, self.y) == (other.x, other.y)
+        except AttributeError as e:
+            return NotImplemented
+
+    # get the position of the agent
     def get_pos(self):
         return (self.x, self.y)
 
+    # translates direction to new coordinates
     def get_new_coords(self, direction):
         if direction in ["N", 0]:
             return (self.x, self.y - 1)
@@ -359,15 +460,20 @@ class Agent:
         if direction in ["W", 3]:
             return (self.x - 1, self.y)
 
+    # if the agent is on a burning cell, the agent dies
     def update(self):
         if self.env.world[self.x][self.y].burning:
             print("Agent has died")
             return "Dead"
 
+    # moves in direction if the cell at the new coordinates is 
+    # traversable, inbounds, and not occupied by another agent
     def move(self, direction):
         (newX, newY) = self.get_new_coords(direction)
-        if self.env.inbounds(newX, newY) and self.env.traversable(newX, newY):
-            (self.x, self.y) = (newX, newY)
+        if (newX, newY) not in [a.get_pos() for a in self.env.agents]:
+            if self.env.inbounds(newX, newY) and self.env.traversable(newX, newY):
+                (self.x, self.y) = (newX, newY)
 
+    # convert cell under the agent to dirt
     def dig(self):
         self.env.set_at(self.x, self.y, Dirt(self.x, self.y))
