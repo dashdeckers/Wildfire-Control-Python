887a526fe2908ee088d238a04fad22ef60a940b0
Travis Hammond <dashdeckers@gmail.com>
2019-05-25 10:57:46 +0200

    10k is enough maybe

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ if len(sys.argv) > 1 and sys.argv[1] == "-run":
     if len(sys.argv) > 2:
         DQN = DQN(forestfire, sys.argv[2])
         DQN.collect_memories(100)
-        DQN.learn(20000)
+        DQN.learn(10000)
     # Run without a name
     else:
         DQN = DQN(forestfire)
         DQN.collect_memories(100)
-        DQN.learn(20000)
+        DQN.learn(10000)
 # Don't run, just create the DQN
 else:
     DQN = DQN(forestfire)
