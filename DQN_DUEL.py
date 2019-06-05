4bcbfa615372c53f26ec420758e18d87f1b286ce
Dirk Jelle Schaap <dirkjelleschaap@gmail.com>
2019-06-05 13:27:36 +0200

    I am stupid

---
+++ DQN_DUEL.py
@@ -0,0 +1,46 @@
+from DQN import DQN
+from collections import deque
+
+import numpy as np
+import time, random
+
+# Dueling DQN specific imports
+from keras.models import Model
+from keras.layers import Input, Flatten, Dense, Lambda, Activation
+from keras.optimizers import Adam
+from keras import backend as K
+
+class DQN_DUEL(DQN):
+    def __init__(self, sim, name="no_name"):
+        DQN.__init__(self, sim, name)
+
+    def make_network(self):
+        input_shape = (self.sim.W.WIDTH, self.sim.W.HEIGHT, self.sim.W.DEPTH)
+        
+        # Create input layer according to input_shape and flatten it
+        input_layer = Input(shape = input_shape)
+        flatten = Flatten()(input_layer)
+
+        # Advantage stream connected to flatten, output size is action_size
+        dense1 = Dense(units=50, activation='sigmoid')(flatten)
+        advantage = Dense(self.action_size)(dense1)
+
+        # Value stream connected to flatten, output size is 1
+        dense2 = Dense(units=1, activation='sigmoid')(flatten)
+        value = Dense(1)(dense2)
+
+        # Combine both streams using: q = a - mean(a) + v
+        output_layer = Lambda(
+                lambda x: x[0]-K.mean(x[0])+x[1],
+                output_shape = (self.action_size,)
+            )([advantage, value])
+        output_layer = Activation('linear')(output_layer)
+
+        # Create model by setting input and output
+        model = Model(input=[input_layer], output=[output_layer])
+        # Copied from DQN, need to understand more
+        model.compile(loss='mse', optimizer=Adam(lr=self.alpha, clipvalue=1))
+
+        # Print summary and return resulting model
+        model.summary()
+        return model
