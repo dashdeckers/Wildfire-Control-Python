04a80c2c17ae030126598cd6c51a1f1347457e8b
Travis Hammond <dashdeckers@gmail.com>
2019-06-17 14:16:36 +0200

    Added BOTH as an algorithm that combines DUEL and SARSA

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ parser.add_argument("-e", "--episodes", type=int, default=10000,
                     help="Number of episodes / runs to learn for")
 
 parser.add_argument("-t", "--type", type=str, default="DQN",
-                    choices=["DQN", "SARSA", "DDQN", "Human"],
+                    choices=["DQN", "SARSA", "DDQN", "BOTH", "Human"],
                     help="The algorithm to use")
 
 parser.add_argument("-n", "--name", type=str, default="no_name",
@@ -47,6 +47,8 @@ if args.run:
         from DQN_SARSA import DQN_SARSA as DQN
     if args.type == "DDQN":
         from DQN_DUEL import DQN_DUEL as DQN
+    if args.type == "BOTH":
+        from DQN_BOTH import DQN_BOTH as DQN
 
     Agent = DQN(forestfire, args.name)
     Agent.collect_memories(args.memories)
@@ -64,11 +66,13 @@ else:
         from DQN import DQN
         from DQN_SARSA import DQN_SARSA
         from DQN_DUEL import DQN_DUEL
+        from DQN_BOTH import DQN_BOTH
 
         # Create the agents
         DQN = DQN(forestfire, verbose=False)
         DQN_SARSA = DQN_SARSA(forestfire, verbose=False)
         DQN_DUEL = DQN_DUEL(forestfire, verbose=False)
+        DQN_BOTH = DQN_BOTH(forestfire, verbose=False)
 
         # Get a list of imported algorithms to play with
         options = [o for o in dir() \
